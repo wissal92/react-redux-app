4399188776752060ca87a3f5823163920c49921f
wissal <wissal.el@gmail.com>
2019-09-23 22:22:44 -0700

    Add edit and add functionalities

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,60 +1,146 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { bindActionCreators } from "redux";
-import PropTypes from 'prop-types';
-import * as courseActions from '../../redux/actions/courseActions';
-import * as authorActions from '../../redux/actions/authorActions';
-import Courselist from './CourseList'
+// import React from "react";
+// import { connect } from "react-redux";
+// import * as courseActions from "../../redux/actions/courseActions";
+// import * as authorActions from "../../redux/actions/authorActions";
+// import PropTypes from "prop-types";
+// import { bindActionCreators } from "redux";
+// import CourseList from "./CourseList";
 
-class CoursesPage extends Component{   
-    componentDidMount() {
-        const { actions, courses, authors } = this.props;
-        
-          if(courses.length === 0){
-            actions.loadCourses().catch(error => {
-              alert("Loading courses failed" + error);
-            });
-          }
-          
-          if(authors.length === 0){
-            actions.loadAuthors().catch(error => {
-              alert("Loading authors failed" + error);
-            });
-          }
+// class CoursesPage extends React.Component {
+//   componentDidMount() {
+//     const { courses, authors, actions } = this.props;
+
+//     if (courses.length === 0) {
+//       actions.loadCourses().catch(error => {
+//         alert("Loading courses failed" + error);
+//       });
+//     }
+
+//     if (authors.length === 0) {
+//       actions.loadAuthors().catch(error => {
+//         alert("Loading authors failed" + error);
+//       });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <h2>Courses</h2>
+//         <CourseList courses={this.props.courses} />
+//       </>
+//     );
+//   }
+// }
+
+// CoursesPage.propTypes = {
+//   authors: PropTypes.array.isRequired,
+//   courses: PropTypes.array.isRequired,
+//   actions: PropTypes.object.isRequired
+// };
+
+// function mapStateToProps(state) {
+//   return {
+//     courses:
+//       state.authors.length === 0
+//         ? []
+//         : state.courses.map(course => {
+//             return {
+//               ...course,
+//               authorName: state.authors.find(a => a.id === course.authorId).name
+//             };
+//           }),
+//     authors: state.authors
+//   };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     actions: {
+//       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+//       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+//     }
+//   };
+// }
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(CoursesPage);
+
+
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
+
+class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
+
+  componentDidMount() {
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses().catch(error => {
+        alert("Loading courses failed" + error);
+      });
     }
-    
-    render() {
-        return (
-          <>
-            <h2>Courses</h2>
-            <Courselist courses={this.props.courses} />
-          </>
-        );
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
+      });
     }
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
+        <h2>Courses</h2>
+
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
+
+        <CourseList courses={this.props.courses} />
+      </>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    authors: PropTypes.array.isRequired,
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state =>{
-    return {
-        courses:
-          state.authors.length === 0
-            ? []
-            : state.courses.map(course => {
-                return {
-                  ...course,
-                  authorName: state.authors.find(a => a.id === course.authorId).name
-                };
-              }),
-        authors: state.authors
-    };
+function mapStateToProps(state) {
+  return {
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
+  };
 }
 
-const mapDispatchToProps = dispatch => {
+function mapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
@@ -63,4 +149,11 @@ const mapDispatchToProps = dispatch => {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
+
+
+
+
